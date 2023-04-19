7ab763262db1bc275c647df88df3fab31454c784
Aseempaul007 <aseempal007@gmail.com>
2023-04-19 13:51:51 +0530

    Learned if-else in kotlin!

---
+++ basic/src/main/java/com/example/basic/IfElseStatements.kt
@@ -0,0 +1,23 @@
+package com.example.basic
+
+fun main(){
+
+    println("Please enter yor age: ")
+    var marks : Int? = readLine()?.toInt();
+
+    var myValue : String = if(marks!! >90){
+        "Grade : A"
+    }else if (marks!!>80){
+        "Grade : B"
+    }else if (marks!!>70){
+        "Grade : C"
+    }else if (marks!!>60){
+        "Grade : D"
+    }else if (marks!!>50){
+        "Grade : E"
+    }else{
+        "Fail"
+    }
+
+    println(myValue)
+}
